d7808a7e20e031eb5f034e3bf84a1bf79e4ac54e
Chocobe <nice_10bird@naver.com>
2019-07-08 21:27:39 +0900

    <master> : Created max_val_2

--- Data_structure/src/basic_algorithm/max_val_2/Calculator.java
+++ Data_structure/src/basic_algorithm/max_val_2/Calculator.java
@@ -1,6 +1,7 @@
 package basic_algorithm.max_val_2;
 
 public class Calculator {
+	// 4개의 정수 중, 최대값 구하기
 	public static int find_max(int _a, int _b, int _c, int _d) {
 		int max = _a;
 		
@@ -11,5 +12,31 @@ public class Calculator {
 		if(max < _d) { max = _d; }
 		
 		return max;
+	}
+	
+	
+	// 3개 정수 중, 최소값 구하기
+	public static int find_min(int _a, int _b, int _c) {
+		int min = _a;
+		
+		if(min > _b) { min = _b; }
+		
+		if(min > _c) { min = _c; }
+		
+		return min;
+	}
+	
+	
+	// 4개 정수 중, 최소값 구하기
+	public static int find_min(int _a, int _b, int _c, int _d) {
+		int min = _a;
+		
+		if(min > _b) { min = _b; }
+		
+		if(min > _c) { min = _c; }
+		
+		if(min > _d) { min = _d; }
+		
+		return min;
 	}
 }
--- Data_structure/src/basic_algorithm/max_val_2/Max_tester.java
+++ Data_structure/src/basic_algorithm/max_val_2/Max_tester.java
@@ -8,14 +8,24 @@ public class Max_tester {
 		
 		int[] input_data = new int[4];
 		int max = 0;
+		int min = 0;
 		
 		for(int i = 0; i < input_data.length; i++) {
 			System.out.print("정수입력 : ");
 			input_data[i] = scanner.nextInt();
 		}
 		
+		// 4개 정수 중, 최대값 테스트
 		max = Calculator.find_max(input_data[0], input_data[1], input_data[2], input_data[3]);
-		System.out.println("최대값 : " + max);
+		System.out.println("4개 항 중 최대값 : " + max);
+		
+		// 3개 정수 중, 최소값 테스트
+		min = Calculator.find_min(input_data[0], input_data[1], input_data[2]);
+		System.out.println("3개 항 중 최소값 : " + min);
+		
+		// 4개 정수 중, 최소값 테스트
+		min = Calculator.find_min(input_data[0], input_data[1], input_data[2], input_data[3]);
+		System.out.println("4개 항 중 최소값 : " + min);
 		
 		scanner.close();
 	}
