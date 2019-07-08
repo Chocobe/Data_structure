8ed209b464e11380c639782191d72cf9dbeb6489
Chocobe <nice_10bird@naver.com>
2019-07-08 23:30:57 +0900

    <master> : Fixed sum_to_n

--- Data_structure/src/basic_algorithm/sum_to_n/Calculator.java
+++ Data_structure/src/basic_algorithm/sum_to_n/Calculator.java
@@ -28,4 +28,51 @@ public class Calculator {
 		
 		return sum;
 	}
+	
+	
+	// n까지의 합 구하기 (연산식 출력)
+	public static int sum_to_n_print_process(int _n) {
+		int sum = 0;
+		int i = 1;
+		
+		while(i < _n) {
+			sum += i;
+			System.out.print(i + " + ");
+			i++;
+		}
+		
+		System.out.print(i + " = ");
+		sum += i;
+		
+		return sum;
+	}
+	
+	
+	// n까지의 합 구하기 (가우스 덧셈)
+	public static int sum_to_n_gauss(int _n) {
+		return _n * (_n + 1) / 2;
+	}
+	
+	
+	// 두 정수를 포함하는 사이값들의 총 합 구하기
+	public static int sumof(int _lhs, int _rhs) {
+		int begin;
+		int end;
+		int sum = 0;
+		
+		if(_lhs > _rhs) {
+			begin = _rhs; 
+			end = _lhs;
+			
+		} else {
+			begin = _lhs;
+			end = _rhs;
+		}
+		
+		for(int i = begin; i <= end; i++) {
+			sum += i;
+		}
+		
+		return sum;
+	}
 }
--- Data_structure/src/basic_algorithm/sum_to_n/Sum_tester.java
+++ Data_structure/src/basic_algorithm/sum_to_n/Sum_tester.java
@@ -12,13 +12,48 @@ public class Sum_tester {
 		input_num = scanner.nextInt();
 		sum = Calculator.sum_to_n(input_num);
 		
+		// n까지 총 합 구하기
+		//
 		System.out.println(input_num + "까지의 총 합 : " + sum);
 		
-		System.out.println("-----------------------");
+		print_line();
 		
+		// n까지 총 합 구하기 (마지막 루프값 출력)
+		//
 		sum = Calculator.sum_to_n_print_last_val(input_num);
 		System.out.println(input_num + "까지의 총 합 : " + sum);
 		
+		print_line();
+		
+		// n까지 총 합 구하기 (연산식 출력)
+		//
+		sum = Calculator.sum_to_n_print_process(input_num);
+		System.out.println(sum);
+		
+		print_line();
+		
+		// n까지 총 합 구하기 (가우스 덧셈)
+		sum = Calculator.sum_to_n_gauss(input_num);
+		System.out.println(input_num + "까지 총 합(가우스 덧셈) : " + sum);
+		
+		print_line();
+		
+		// 두 정수 사이값의 총 합 구하기
+		//
+		System.out.print("정수입력 : ");
+		int lhs = scanner.nextInt();
+		
+		System.out.print("정수입력 : ");
+		int rhs = scanner.nextInt();
+		
+		sum = Calculator.sumof(lhs, rhs);
+		System.out.println(lhs + "과(와) " + rhs + "사이의 정수의 합 : " + sum);
+		
 		scanner.close();		
+	}
+	
+	
+	public static void print_line() {
+		System.out.println("-----------------------");
 	}
 }
